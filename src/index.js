fcf991d6220854c1addaf3a2a75c9471422f2b15
yingzibiyanggua <1070179206@qq.com>
2019-03-29 16:29:38 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import 'amfe-flexible';
 import { join } from 'lodash';
 import './css/index.css';
 let div = document.createElement('div');
-console.log('12123');
 div.innerText = join(['hello', 'webpack'], ' ');
 document.getElementsByTagName('body')[0].appendChild(div);
